15d10b296fd4844537709c37d17dd4864d5e5a25
FrankW1994 <FrankW1994@users.noreply.github.com>
2020-02-12 12:25:15 +0100

    Dit is de opzet voor het hele project hf. Werk in je eigen branch en probeer niet de master te verpesten ;P

--- DAL/Database.php
+++ DAL/Database.php
@@ -11,16 +11,15 @@ define('DB_NAME', 'hfa3_db');
 
 class Database
 {
-    protected function dbconnenct(){
-        try
-        {
-            $dsn = "mysql:host=".DB_HOST.";dbname=".DB_NAME;
+    protected function dbconnenct()
+    {
+        try {
+            $dsn = "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME;
             $pdo = new PDO($dsn, DB_USER, DB_PASS);
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $pdo;
+        } catch (PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
         }
-        catch (PDOException $e)
-        {
-            echo "Connection failed: ".$e->getMessage();
-        }
+    }
 }
