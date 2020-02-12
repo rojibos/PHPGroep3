78066574d33541a3aecd315284c7e845ef894458
FrankW1994 <FrankW1994@users.noreply.github.com>
2020-02-12 14:30:35 +0100

    css enz

--- views/dance/dance.php
+++ views/dance/dance.php
@@ -2,7 +2,8 @@
 require_once '../header.php';
 ?>
 <main>
-
+    Dit is the dance page.
 </main>
 <?php
 require_once '../footer.php';
+?>
--- views/header.php
+++ views/header.php
@@ -1,13 +1,21 @@
 <html>
 <head>
     <title>Haarlem Festival</title>
-    <link rel="stylesheet" href="../public/css/default.css">
+    <link rel="stylesheet" type="text/css" href="../public/css/default.css">
 </head>
 <body>
 <header>
     <nav id="mainNav">
+        <a href="instagram.com" class="socialLink">insta</a>
+        <a href="facebook.com" class="socialLink">fb</a>
+        <a href="twitter.com" class="socialLink">twitter</a>
         <ul>
-            <li><a href=""></a></li>
+            <li><a href="index/index.php">Home</a></li>
+            <li><a href="index/index.php">Events</a></li>
+            <li><a href="index/index.php">Programme</a></li>
+            <li><a href="index/index.php">Tickets</a></li>
+            <li><a href="index/index.php">Contact</a></li>
         </ul>
+        <a href="login/login.php">Sign in</a>
     </nav>
 </header>
