7b7b8fdd9d6f81724b476bcd5bf3c45ef771416f
frank wester <wester.frank@hotmail.com>
2020-02-13 13:19:06 +0100

    css navigation updated

--- views/header.php
+++ views/header.php
@@ -1,21 +1,48 @@
+<?php
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+    $_SESSION['username'] = 'frank';
+}
+?>
 <html>
 <head>
     <title>Haarlem Festival</title>
-    <link rel="stylesheet" type="text/css" href="../public/css/default.css">
+    <link rel="stylesheet" type="text/css" href="../../public/css/default.css">
 </head>
 <body>
 <header>
     <nav id="mainNav">
-        <a href="instagram.com" class="socialLink">insta</a>
-        <a href="facebook.com" class="socialLink">fb</a>
-        <a href="twitter.com" class="socialLink">twitter</a>
+        <a href="http://instagram.com" class="socialLink"><img src="../../public/images/insta.png"></a>
+        <a href="http://facebook.com" class="socialLink"><img src="../../public/images/facebook.png"></a>
+        <a href="http://twitter.com" class="socialLink"><img src="../../public/images/twitter.png" alt=""></a>
+        <a href="index/index.php" id="logo">HAARLEM FESTIVAL</a>
+
+        <?php
+        if (isset($_SESSION['username'])){
+            ?>
+                <div class="userMenu">
+                    <button class="userMenuBtn"><img src="../../public/images/usermenu.png"></button>
+                    <div class="userMenuContent">
+                        <a href="dance/dance.php">profile</a>
+                        <a href="dance/dance.php">logout</a>
+                    </div>
+                </div>
+            <?php
+        }
+        else{
+            echo '<a href="login/login.php" id="login">Sign in</a>';
+        }
+        ?>
+
+
+        <a href="cart/cart.php"><img src="../../public/images/cart.png" id="cartBtn"></a>
         <ul>
-            <li><a href="index/index.php">Home</a></li>
-            <li><a href="index/index.php">Events</a></li>
-            <li><a href="index/index.php">Programme</a></li>
-            <li><a href="index/index.php">Tickets</a></li>
-            <li><a href="index/index.php">Contact</a></li>
+            <li><a href="dance/dance.php">Contact</a></li>
+            <li><a href="dance/dance.php">Tickets</a></li>
+            <li><a href="dance/dance.php">Programme</a></li>
+            <li><a href="dance/dance.php">Events</a></li>
+            <li><a href="dance/dance.php">Home</a></li>
         </ul>
-        <a href="login/login.php">Sign in</a>
+
     </nav>
 </header>
