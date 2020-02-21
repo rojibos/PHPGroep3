27a19f6102a6781954b890c1fbeac76cd510f53a
frank wester <wester.frank@hotmail.com>
2020-02-21 13:31:50 +0100

    program start

---
+++ views/program/program.php
@@ -0,0 +1,23 @@
+<?php
+require_once '../header.php';
+?>
+    <main>
+        <section class="program">
+            <table class="programTable">
+                <tr class="top">
+                    <th>Friday</th>
+                </tr>
+                <tr>
+                    <td><a href="../tickets/tickets.php?type=dance&scroll=friday"><p>Tiesto<br>1200<br>Lichtfabriek</p></a></td>
+                </tr>
+                <tr>
+                    <td><a href="../tickets/tickets.php?type=dance&scroll=friday"><p>Tiesto<br>1200<br>Lichtfabriek</p></a></td>
+                </tr>
+                <tr>
+                    <td><a href="../tickets/tickets.php?type=dance&scroll=friday"><p>Tiesto<br>1200<br>Lichtfabriek</p></a></td>
+                </tr>
+            </table>
+        </section>
+    </main>
+<?php
+require_once '../footer.php';
