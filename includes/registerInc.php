56ade98a99e7b91b6248f9266504594529568658
frank wester <wester.frank@hotmail.com>
2020-03-02 17:28:46 +0100

    merger

--- includes/registerInc.php
+++ includes/registerInc.php
@@ -1,5 +1,58 @@
 <?php
+require '../../logic/User.php';
+require '../../DAL/User_DAL.php';
+require '../../logic/Mail.php';
 if (isset($_POST['register']))
 {
+    $username = $_POST['username'];
+    $email = $_POST['usermail'];
+    $password = $_POST['userpassword'];
+    $passwordRepeat = $_POST['userpasswordretype'];
+    $birthday = $_POST['userbirthday'];
+    $currentDate = date('Y-m-d');
 
+    if (empty($username) || empty($email) || empty($password) || empty($passwordRepeat) || empty($birthday))
+    {
+        header("Location: ../view/register.php?error=emptyfields");
+        exit();
+    }
+    elseif (!filter_var($email, FILTER_VALIDATE_EMAIL))
+    {
+        header("Location: ../view/register.php?error=invalidemail");
+        exit();
+    }
+    elseif ($birthday > $currentDate)
+    {
+        header("Location: ../view/register.php?error=invaliddate");
+        exit();
+    }
+    elseif ($password != $passwordRepeat)
+    {
+        header("Location: ../view/register.php?error=passwordnotequal");
+        exit();
+    }
+    else
+    {
+        $user = new User();
+        $mail = new Mail();
+        if (!$user->usernameCheck($username)){
+            header("Location: ../view/register.php?error=usernameunavaileble");
+            exit();
+        }
+        elseif ($user->emailCheck($email)){
+            header("Location: ../view/register.php?error=emailunavaileble");
+            exit();
+        }
+        else{
+            $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+            $user->register($username, $email, $birthday, $hashedPassword, $currentDate);
+            $mail->conformationMail();
+            header("Location: ../view/succes.php?succes=registered");
+            exit();
+        }
+    }
+}
+else{
+    header("Location: ../view/register.php");
+    exit();
 }
--- logic/User.php
+++ logic/User.php
@@ -1,7 +1,18 @@
 <?php
-
+require '../../DAL/User_DAL.php';
 
 class User
 {
+    private $DAL;
 
+    function __construct()
+    {
+        $this->DAL = new User_DAL();
+
+    }
+    function register($username, $email, $birthday, $hashedPassword, $currentdate)
+    {
+        $this->addUser($username, $email, $birthday, $hashedPassword, $currentdate);
+        return;
+    }
 }
