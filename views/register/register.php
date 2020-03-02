f03f7325a344aa95f679cd9a3373c2eab9c42c95
FrankW1994 <FrankW1994@users.noreply.github.com>
2020-03-02 13:40:49 +0100

    register form

--- views/register/register.php
+++ views/register/register.php
@@ -2,9 +2,16 @@
 require_once '../header.php';
 ?>
 <main>
-    <form action="../../includes/registerInc.php" method="post">
-
-    </form>
+    <section class="standardFormWrapper">
+        <h2>Contact form</h2>
+        <form action="../../includes/registerInc.php" class="standardForm" method="post">
+            Name: <input type="text" name="username" placeholder="Name">
+            E-mail: <input type="text" name="usermail" placeholder="Your E-mail address">
+            Date of birth: <input type="date" name="userbirthday">
+            Password: <input type="password" name="userpassword">
+            Retype Password <input type="password" name="userpasswordretype">
+            <input type="submit" name="register" value="Register">
+        </form>
 </main>
 <?php
 require_once '../footer.php';
