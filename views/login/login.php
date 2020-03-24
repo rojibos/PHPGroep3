dfe5031113db2651f41ed8d9e356aff1724f224c
frank wester <wester.frank@hotmail.com>
2020-03-24 14:05:08 +0100

    acces to registration registration

--- views/login/login.php
+++ views/login/login.php
@@ -7,6 +7,7 @@ require_once '../../includes/loginInc.php';
             <h2>Login</h2>
             <form action="../../includes/loginInc.php" class="standardForm" method="post">
                 Name: <input type="text" name="username" placeholder="Name"><br>
+                <a href="password-reset.php" id="wwReset">Forgot password?</a><br>
                 Password: <input type="password" name="userpassword" placeholder="******"><br>
                 <input type="submit" name="login" value="Login">
                 <input type="submit" name="register" value="Register account">
