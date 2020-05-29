2f40db11c5ef29090ee30c9e9196c38e6ba1282f
frank wester <wester.frank@hotmail.com>
2020-05-29 16:18:31 +0200

    images en fixes

--- DAL/Database.php
+++ DAL/Database.php
@@ -3,7 +3,7 @@ define('DB_HOST','localhost');
 define('DB_USER', 'root');
 define('DB_PASS','');
 define('DB_NAME', 'haarlemfestival');
-
+//
 //define('DB_HOST','localhost');
 //define('DB_USER', 'hfa3');
 //define('DB_PASS', 'vlbEv6Vs');
--- views/dance.php
+++ views/dance.php
@@ -7,38 +7,28 @@ $data = $content->GetAllContent('dance');
     <main>
         <!--    Hier roep ik variabele aan die in de logica laag uit de database worden gehaald, ik zet ze dan in de juiste elementen.-->
         <div class = "container">
-            <section id="headerjazz">
+            <section id="headerdance">
                 <h1 class="titel"><?php echo $data[0]->text; ?></h1>
+                <h2 class="underTitel"><?php echo $data[1]->text; ?></h2>
             </section>
             <section class="inleiding">
-                <h2><?php  ?></h2>
-                <p><?php echo $data[1]->text; ?></p>
+                <h1><?php  ?></h1>
+                <p><?php echo $data[2]->text; ?></p>
             </section>
-
-            <article id="mainhall">
-                <h2><?php echo $data[2]->text; ?></h2>
+            <article id="Lineup">
+                <h1><?php echo $data[3]->text; ?></h1>
                 <hr>
-                <p><?php  echo $data[3]->text;  ?></p>
+                <p><?php  echo $data[4]->text;  ?></p>
             </article>
-            <article id="secondhall">
-                <h2><?php  echo $data[4]->text; ?></h2>
+            <article id="venues">
+                <h1><?php  echo $data[5]->text; ?></h1>
                 <hr>
-                <p><?php  echo $data[5]->text; ?></p>
-            </article>
-            <article id="thirdhall">
-                <h2><?php  echo $data[6]->text; ?></h2>
-                <hr>
-                <p><?php  echo $data[7]->text; ?></p>
-            </article>
-            <article id="grotemarkt">
-                <h2><?php  echo $data[8]->text; ?></h2>
-                <hr>
-                <p><?php  echo $data[9]->text; ?></p>
+                <p><?php  echo $data[6]->text; ?></p>
             </article>
             <!--        Hier maak ik twee knoppen die naar de programpage en ticketpage gaan. Ik geef een variabele event mee in de url zodat op de ticketpagina direct naar het jazz event kan worden genavigeerd.-->
             <section class="navigationbuttons">
                 <div id="forms">
-                    <form action="../tickets/ticketpage.php?event=jazz">
+                    <form action="tickets.php?event=dance">
                         <button type="submit" class ="gototicketpage" id="gototicketpagejazz">TICKETS</button>
                     </form>
                     <form action="program.php">
